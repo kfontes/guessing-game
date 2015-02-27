e46424fdc42290fe57c97202d25afad81a02a5f1
Kellie <kfontes.uw@gmail.com>
2015-02-27 15:19:42 -0800

    adding .html, .css and .js files

---
+++ guess.js
@@ -0,0 +1,30 @@
+$(function(){
+  event.preventDefault();
+  var compGuess = Math.floor((Math.random() * 10) + 1), userGuess, i = 0;
+  console.log(compGuess);
+  
+  $("#guess").submit(function(){
+
+    userGuess = $("#userGuess").val();
+
+    if (userGuess == compGuess){
+      $("#result").html("Congratulations! '" + userGuess + "' was the right number. Click the button below to play again.");
+      $("<button>PLAY AGAIN</button>").insertAfter("#result");
+      $("button").addClass("pure-button").click(function(){
+        location.reload();
+      });
+
+    } else {
+      $("#result").html("Sorry, '" + userGuess + "' was not what the computer guessed. Keep trying!");
+      $("#userGuess").val("");
+      if(i==0){
+        $('#pastGuesses').text("Previous guesses:").append(" " + userGuess);
+        i++;
+      } else {
+        $("#pastGuesses").append(", " + userGuess); 
+        i++;
+      }
+    }
+    event.preventDefault();
+  });  
+});
