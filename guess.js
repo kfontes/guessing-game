05e8de10b9d82036e48c41d9d26280c6ac324b90
kfontes <kfontes.uw@gmail.com>
2015-06-14 17:26:14 -0700

    switching === to ==

--- guess.js
+++ guess.js
@@ -7,7 +7,7 @@ $(function(){
 
     userGuess = $("#userGuess").val();
 
-    if (userGuess === compGuess){
+    if (userGuess == compGuess){
       $("#result").html("Congratulations! '" + userGuess + "' was the right number. Click the button below to play again.");
       $("<button>PLAY AGAIN</button>").insertAfter("#result");
       $("button").addClass("pure-button").click(function(){
@@ -16,7 +16,7 @@ $(function(){
     } else {
       $("#result").html("Sorry, '" + userGuess + "' was not what the computer guessed. Keep trying!");
       $("#userGuess").val("");
-      if(i===0){
+      if(i==0){
         $('#pastGuesses').text("Previous guesses:").append(" " + userGuess);
         i++;
       } else {
